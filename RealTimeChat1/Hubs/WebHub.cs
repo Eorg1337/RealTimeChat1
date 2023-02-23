3e09d2cfb2661a6f0ccb29bdad9b501bcdc8f3fa
Sergei Kazankov <sergeikazankov12@gmail.com>
2023-02-23 15:31:08 +0300

    MoreAbstractions
    Струкутура переделана под струкутуру сообщения Message.
    
    MessageContainer по Callback получается список сообщений из MessageController

---
+++ RealTimeChat1/Hubs/WebHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using RealTimeChat1.Shared;
+
+namespace RealTimeChat1.Hubs
+{
+	public class WebHub : Hub
+	{
+		public async Task SendMessage(Message _message)
+			=> await Clients.All.SendAsync("ReceiveMessage", _message);
+	}
+}
---
+++ RealTimeChat1/Shared/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace RealTimeChat1.Shared
+{
+	public class Message
+	{
+		public string UserImage { get; set; }
+		public string UserName { get; set; }
+		public string MessageText { get; set; }
+		public string SendTime { get; set; }
+
+		public Message(string userImage, string userName, string messageText, string sendTime)
+		{
+			UserImage = userImage;
+			UserName = userName;
+			MessageText = messageText;
+			SendTime = sendTime;	
+		}
+	}
+}
